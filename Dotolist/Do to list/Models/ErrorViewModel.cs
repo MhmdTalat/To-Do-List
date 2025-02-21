2ec1ad49fae6aacee1d96529f082ee3cb799ebaf
MhmdTalat <>
2025-02-21 20:46:10 +0200

    To-Do List is a web-based task management platform designed to help users stay organized, boost productivity, and efficiently manage their daily schedules. Whether you're tracking personal tasks, work assignments, or long-term projects, To-Do List provides a simple yet effective solution

--- Dotolist/Do to list/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Do_to_list.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
