d800f2f550e84ca07173080300638f811a17d771
MhmdTalat <>
2025-02-21 22:11:10 +0200

    FoodMart - Your Ultimate Online Food Shopping Experience

--- ECommerceProject/ECommerceProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ECommerceProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
